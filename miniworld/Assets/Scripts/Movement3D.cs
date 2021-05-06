56343e4091abcef026d956123fd35165d1f91020
Dayoung Lee <63578441+dayoung33@users.noreply.github.com>
2021-05-06 16:10:27 +0900

    오르는 거 속도 수정

--- miniworld/Assets/Scripts/Movement3D.cs
+++ miniworld/Assets/Scripts/Movement3D.cs
@@ -62,7 +62,7 @@ public class Movement3D : MonoBehaviour
 
     public void ClimbTo()
     {
-        moveDirection.y = 0.3f;
+        moveDirection.y = 10.0f*Time.deltaTime;
     }
 
     public void ClimbEnd()
